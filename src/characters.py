90209acd70f4c97135fcdae8ca98798b002ae392
Suzanna Wentzel <suzannawentzel1708@gmail.com>
2021-12-03 20:12:48 +0100

    feat: adds another list that removes zero occurences from character list

--- src/characters.py
+++ src/characters.py
@@ -7,7 +7,7 @@ work_sheet = workbook.active
 characters = [work_sheet[i][1].value for i in range(1, 451)]
 
 
-# process fairytale dataset into one string
+# Process fairytale dataset into one string
 fairy_tales = ''
 with open('../data/Fairytales_db/merged_clean.txt') as file:
     lines = file.readlines()
@@ -17,13 +17,20 @@ with open('../data/Fairytales_db/merged_clean.txt') as file:
 
 
 # For each character, count how many times it occurs in fairytale dataset
-counted_characters = [[character, fairy_tales.count(character)] for character in characters]
+counted_characters = [[character, fairy_tales.count(character)] for character in characters if fairy_tales.count(character) > 0]
 counted_characters = sorted(counted_characters, key=lambda x: x[1], reverse=True)
 print(counted_characters)
 
 
-# write 15 most used characters to file
+# Write all left over characters to file
 f = open("../data/results/characters.txt", "w")
+for i in range(0, len(counted_characters)):
+    f.write(str(i + 1) + '. ' + counted_characters[i][0] + ' : ' + str(counted_characters[i][1]) + ' times\n')
+f.close()
+
+
+# Write 15 most used characters to file
+f = open("../data/results/15_characters.txt", "w")
 for i in range(0, 15):
     f.write(str(i + 1) + '. ' + counted_characters[i][0] + ' : ' + str(counted_characters[i][1]) + ' times\n')
 f.close()
