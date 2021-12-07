a7863dd96700b8dfae923a26c22a26a38d9abb71
viremaster <rikdesain@gmail.com>
2021-12-07 14:07:16 +0100

    Imported Quiriens code. Added seperation of values.

---
+++ quiriens_code.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from collections import Counter
+
+df = pd.read_excel('./data/Veale_db/Veale_s script midpoints.xlsx', index_col=None, na_values=['NA'])
+verbs_after_mp = df['After Midpoint'].tolist()
+
+new_verbs_after_mp = []
+for i in verbs_after_mp:
+    for verb in i.replace(" ", "").split(","):
+        new_verbs_after_mp.append(verb)
+verbs_after_mp = new_verbs_after_mp
+
+d = dict()
+
+for verb in verbs_after_mp:
+    if verb in d:
+        d[verb] = d[verb] + 1
+    else:
+        d[verb] = 1
+
+d = Counter(d)
+common_verbs = d.most_common(100)
+print(common_verbs)
