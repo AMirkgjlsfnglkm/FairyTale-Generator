1b533e665c0bb6e0b60c69ca7ee9ef0e4ca39039
Suzanna Wentzel <suzannawentzel1708@gmail.com>
2021-12-08 12:25:13 +0100

    feat: updates character counting file to work properly

--- src/characters.py
+++ src/characters.py
@@ -4,7 +4,7 @@ from openpyxl import load_workbook
 # Get character categories from Veale
 workbook = load_workbook(filename='../data/Veale_db/Veale\'s category actions.xlsx')
 work_sheet = workbook.active
-characters = [work_sheet[i][1].value for i in range(1, 451)]
+characters = [work_sheet[i][1].value for i in range(2, 451)]        # start from 2, don't include header row
 
 
 # Process fairytale dataset into one string
@@ -13,11 +13,11 @@ with open('../data/Fairytales_db/merged_clean.txt') as file:
     lines = file.readlines()
     lines = [line.strip() for line in lines]
     for line in lines:
-        fairy_tales += line
+        fairy_tales += line.lower()
 
 
-# For each character, count how many times it occurs in fairytale dataset
-counted_characters = [[character, fairy_tales.count(character)] for character in characters if fairy_tales.count(character) > 0]
+# For each character, count how many times it occurs in fairytale dataset (adds spaces for 'word bounds')
+counted_characters = [[character, fairy_tales.count(' ' + character.lower() + ' ')] for character in characters if fairy_tales.count(' ' + character.lower() + ' ') > 0]
 counted_characters = sorted(counted_characters, key=lambda x: x[1], reverse=True)
 print(counted_characters)
 
@@ -29,9 +29,9 @@ for i in range(0, len(counted_characters)):
 f.close()
 
 
-# Write 15 most used characters to file
-f = open("../data/results/15_characters.txt", "w")
-for i in range(0, 15):
-    f.write(str(i + 1) + '. ' + counted_characters[i][0] + ' : ' + str(counted_characters[i][1]) + ' times\n')
-f.close()
-
+# Write most used characters to file based on treshold
+# treshold = 15
+# f = open("../data/results/" + str(treshold) + "_characters.txt", "w")
+# for i in range(0, treshold):
+#     f.write(str(i + 1) + '. ' + counted_characters[i][0] + ' : ' + str(counted_characters[i][1]) + ' times\n')
+# f.close()
