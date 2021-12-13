f91030a07247b3117e175277eba77d98c9173e66
viremaster <rikdesain@gmail.com>
2021-12-13 14:33:14 +0100

    Rewrite of the story generation: Added our own need dataset as well as the changed datasets

--- src/story_maker.py
+++ src/story_maker.py
@@ -1,6 +1,47 @@
 import random as rd
 from openpyxl import load_workbook
 
+vowels = ['a', 'e', 'i', 'u', 'o']
+names = ["Sam", "Alex", "Charlie", "Chris", "Frankie", "Jackie", "Kendall"]
+
+
+class Character:
+    name = ""
+    char_type = ""
+    trait = ""
+    article = ""
+
+    def __init__(self, char_type, trait, char):
+        self.create_name()
+        self.char_type = char_type
+        self.trait = trait
+        self.determine_article(char)
+
+    def create_name(self):
+        name = rd.choice(names)
+        names.remove(name)
+        self.name = name
+
+    def determine_article(self, char):
+        if char == "a":
+            article = "a"
+            if self.trait[0] in vowels:
+                article = "an"
+            self.article = article
+        else:
+            article = "a"
+            if self.char_type[0] in vowels:
+                article = "an"
+            self.article = article
+
+    def name_call(self):
+        choice = rd.randint(0, 1)
+        if choice == 0:
+            to_return = "the " + self.char_type
+        else:
+            to_return = self.name
+        return to_return
+
 
 # The story class holds all function needed to create a story,
 # The seed given at creation will be used in the random elements of the story
@@ -10,8 +51,8 @@ class Story:
     intro = ""
     first_midpoint = []
     second_midpoint = []
-    character_a = ("", "")
-    character_b = ("", "")
+    character_a = None
+    character_b = None
     need = ""
     ending = ""
 
@@ -34,25 +75,25 @@ class Story:
 
     # This function creates the first midpoint of the story: a truple of verbs
     def create_second_midpoint(self):
-        # Open the "Script midpoints" sheet
-        workbook = load_workbook(filename="../data/Veale_db/Veale_s script midpoints.xlsx")
+        # Open the "pruned script midpoints" sheet
+        workbook = load_workbook(filename="../data/Own_db/Our midpoints pruned.xlsx")
         work_sheet = workbook.active
 
         # Choose a random row in the worksheet
-        random_index = rd.randint(2, 2761)
+        random_index = rd.randint(1, 229)
 
         # Store the first verb in the row
         midpoint = [work_sheet[random_index][0].value]
 
         # Choose a random one of the second verbs in the row
-        verb2 = work_sheet[random_index][1].value.split(", ")
+        verb2 = work_sheet[random_index][1].value.split(",")
         midpoint.append(rd.choice(verb2))
 
         # Choose a random one of the third verbs in the row
-        verb3 = work_sheet[random_index][2].value.split(", ")
+        verb3 = work_sheet[random_index][2].value.split(",")
         midpoint.append(rd.choice(verb3))
 
-        # Store the first midpoint
+        # Store the second midpoint
         self.second_midpoint = midpoint
 
     # This function creates the second midpoint of the story, a truple of verbs, based upon the first midpoint
@@ -89,7 +130,7 @@ class Story:
             # Store the second midpoint
             self.first_midpoint = midpoint
         else:
-            print("No second midpoint: starting over")
+            print("No first midpoint: starting over")
 
     # This function creates a character A and a trait based upon the first midpoint, as a subject
     def create_character_a(self):
@@ -150,13 +191,14 @@ class Story:
         # If no possible options were found
         if len(options) < 1:
             print("No trait found: taking a default one")
-            trait = "humble"
+            trait = "brave"
         else:
             # Choose a random one of the possible trait options
             trait = work_sheet[rd.choice(options)][0].value
 
-        # Store the character and trait
-        self.character_a = (character, trait)
+        # Store the character, trait and article
+        obj_character = Character(character, trait, "a")
+        self.character_a = obj_character
 
     # This function creates a character B and a trait based upon the first midpoint, as an object
     def create_character_b(self):
@@ -222,13 +264,24 @@ class Story:
             # Choose a random one of the possible trait options
             trait = work_sheet[rd.choice(options)][0].value
 
-        # Store the character and trait
-        self.character_b = (character, trait)
+        obj_character = Character(character, trait, "b")
+        self.character_b = obj_character
 
     # This function creates a need for character A based on the midpoints.
     def create_need(self):
-        # But it does not do anything yet
-        pass
+        workbook = load_workbook(filename="../data/Own_db/Our needs.xlsx")
+        work_sheet = workbook.active
+
+        verb = self.second_midpoint[2]
+
+        need = ""
+        for row in range(2, 8):
+            if work_sheet[row][0].value == verb:
+                need = rd.choice(work_sheet[row][1].value.split(", "))
+        if need == "":
+            print("No need found: taking standard one")
+            need = "A wanted to change their boring ways."
+        self.need = need
 
     # This function generates an ending based on the second midpoint
     def create_ending(self):
@@ -247,25 +300,46 @@ class Story:
         # If no row was found using the verb
         if self.ending == "":
             print("No bookend found: taking standard one")
-            self.ending = "And they lived happily ever after!"
+            self.ending = "and they lived happily ever after!"
 
     # This function prints the story in a readable format.
     def return_story(self):
+        character_a = self.character_a
+        character_b = self.character_b
+
         # Start sentence
-        print(self.intro, "there was a " + self.character_a[1], self.character_a[0])
+        print(self.intro, "there was", character_a.article, character_a.trait, character_a.char_type,
+              "called", character_a.name)
+
         # Introduction of character B
-        print("They knew a", self.character_b[0], "that was", self.character_b[1])
+        print("They knew", character_b.article, character_b.char_type, "that was", character_b.trait,
+              "called", character_b.name)
+
+        # Introduction of need
+        need_intro = (character_a.name_call() + " " + self.need.replace("A", character_a.name_call())
+                      .replace("B", character_b.name_call()))
+        need_intro = need_intro[0].capitalize() + need_intro[1:]
+        print(need_intro)
+
         # First midpoint idiomatized
         for verb in self.first_midpoint:
-            print(self.util_idiomatize(verb).replace("A", "the " + self.character_a[0])
-                  .replace("B", "the " + self.character_b[0]))
+            verb_sentence = self.util_idiomatize(verb).replace("A", character_a.name_call())\
+                .replace("B", character_b.name_call())
+            verb_sentence = verb_sentence[0].capitalize() + verb_sentence[1:]
+            print(verb_sentence)
+
         # Second midpoint idiomatized
         for verb in self.second_midpoint[1:]:
-            print(self.util_idiomatize(verb).replace("A", "the " + self.character_a[0])
-                  .replace("B", "the " + self.character_b[0]))
+            verb_sentence = self.util_idiomatize(verb).replace("A", character_a.name_call())\
+                .replace("B", character_b.name_call())
+            verb_sentence = verb_sentence[0].capitalize() + verb_sentence[1:]
+            print(verb_sentence)
+
         # Ending
-        print(self.ending.replace("A ", "the " + self.character_a[0])
-                  .replace("B ", "the " + self.character_b[0]))
+        ending_sentence = self.ending.replace("A", character_a.name_call())\
+            .replace("B", character_b.name_call())
+        ending_sentence = ending_sentence[0].capitalize() + ending_sentence[1:]
+        print(ending_sentence)
 
     # This function returns the idiomatized version of the input verb
     @staticmethod
@@ -295,6 +369,9 @@ class Story:
         return verb
 
 
-story = Story(5)
+story = Story(14)
 
-# 32 no first character
+# Almost works: 3, 7, 8
+# Bad story: 1, 11, 12
+# Broken: 2, 6, 9, 13, 14
+# Yay?: 5, 10
---
+++ src/utils.py
@@ -0,0 +1,16 @@
+from openpyxl import load_workbook
+
+workbook = load_workbook(filename="../data/Veale_db/Veale_s script midpoints.xlsx")
+work_sheet = workbook.active
+
+possible_verbs = ['are_disrespected_by', 'are_deceived_by', 'kill', 'manipulate', 'fall_in_love_with', 'profit_from']
+
+options = []
+for row in range(2, 2761):
+    for verb in possible_verbs:
+        if verb in work_sheet[row][2].value.split(", "):
+            options.append(row)
+
+options = list(dict.fromkeys(options))
+for i in options:
+    print(work_sheet[i][0].value, "\t", work_sheet[i][1].value[0:], "\t", work_sheet[i][2].value[0:])
